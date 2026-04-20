a20cac2c870e6e478d27569cb7b70b0e4f1b8aee
sssfshv <sssfshv@yandex.ru>
2026-04-20 15:04:23 +0700

    не удалось войти

--- Desktop/sssfshv.13.04/blog/blog/urls.py
+++ Desktop/sssfshv.13.04/blog/blog/urls.py
@@ -4,4 +4,7 @@ from login import views
 urlpatterns = [
    path('add_user/', views.add_user ),
    path('users/', views.users),
+   path('', views.index),
+   path('login/', views.login),
+   path('exit/', views.logout_view),
 ]
--- Desktop/sssfshv.13.04/blog/login/views.py
+++ Desktop/sssfshv.13.04/blog/login/views.py
@@ -19,3 +19,59 @@ def add_user(request) :
     else :
         form = UserForm()
         return render(request, "add_user.html", {'form': form})
+    
+#главная страница
+def index(request):
+    #если в сессии сохранён пользователь - переходим на главную страницу 
+    if request.session.get('user_id'):
+        #получим логин пользователя
+        I = request.session.get('login')
+        #переходим на главную страницу и передаем туда логин 
+        return render(request, 'index.html', {'login': l})
+    #пользователь неавторизироован - переходим на форму входа 
+    else:
+        return redirect('/login/')
+
+#авторизация 
+def login(request):
+    #это get-запрос. нужно показать пустую форму
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        return redirect('/')
+    
+    # авторизация
+def login(request):
+    # это get-запрос. нужно показать пустую форму
+    if request.method == "GET":
+        return render(request, 'login.html')
+    
+    # пришли данные. значит нужно проверить логин/пароль
+    else:
+        login = request.POST.get('login')
+        password = request.POST.get('pas')
+
+        # проверим что пользователь с таким логином вообще существует
+        try:
+            user = User.objects.get(login=login)
+        except User.DoesNotExist:
+            return redirect('/login')
+
+        # пользователь есть. проверим его пароль
+        if password != user.password:
+            return redirect('/login')
+
+        # сохраним данные пользователя в сессию
+        request.session['user_id'] = user.id
+        request.session['login'] = user.login
+
+        return redirect('/')
+    
+#выйти из системы 
+def logout_view(request):
+    #очистим все данные в сессии 
+    request.session.flush()
+    return redirect('/login')
+
+
+    
